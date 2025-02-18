f2134c6fe9f566da1bab19dd84d08ff67dcacca0
Tom Colvin <tom.colvin@apptaura.com>
2025-02-18 21:53:33 +0000

    first commit, of https://github.com/MasterKale/SimpleWebAuthn/tree/master/example converted to use with Firebase Functions

---
+++ functions/src/index.ts
@@ -0,0 +1,13 @@
+/**
+ * Import function triggers from their respective submodules:
+ *
+ * import {onCall} from "firebase-functions/v2/https";
+ * import {onDocumentWritten} from "firebase-functions/v2/firestore";
+ *
+ * See a full list of supported triggers at https://firebase.google.com/docs/functions
+ */
+
+import {onRequest} from "firebase-functions/v2/https";
+import { app } from "./simplewebauthn/index";
+
+export const auth = onRequest(app);
---
+++ functions/src/simplewebauthn/index.ts
@@ -0,0 +1,290 @@
+/**
+ * An example Express server showing off a simple integration of @simplewebauthn/server.
+ *
+ * The webpages served from ./public use @simplewebauthn/browser.
+ */
+
+import express from 'express';
+import session from 'express-session';
+import memoryStore from 'memorystore';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+import {
+  AuthenticationResponseJSON,
+  // Authentication
+  generateAuthenticationOptions,
+  GenerateAuthenticationOptionsOpts,
+  // Registration
+  generateRegistrationOptions,
+  GenerateRegistrationOptionsOpts,
+  RegistrationResponseJSON,
+  VerifiedAuthenticationResponse,
+  VerifiedRegistrationResponse,
+  verifyAuthenticationResponse,
+  VerifyAuthenticationResponseOpts,
+  verifyRegistrationResponse,
+  VerifyRegistrationResponseOpts,
+  WebAuthnCredential,
+} from '@simplewebauthn/server';
+
+import { LoggedInUser } from './example-server';
+
+export const app = express();
+const MemoryStore = memoryStore(session);
+
+const {
+  ENABLE_CONFORMANCE,
+  RP_ID = 'auth.tomcolvin.co.uk',
+} = process.env;
+
+app.use(express.static('./public/'));
+app.use(express.json());
+app.use(
+  session({
+    secret: 'secret123',
+    saveUninitialized: true,
+    resave: false,
+    cookie: {
+      maxAge: 86400000,
+      httpOnly: true, // Ensure to not expose session cookies to clientside scripts
+    },
+    store: new MemoryStore({
+      checkPeriod: 86_400_000, // prune expired entries every 24h
+    }),
+  }),
+);
+
+/**
+ * If the words "metadata statements" mean anything to you, you'll want to enable this route. It
+ * contains an example of a more complex deployment of SimpleWebAuthn with support enabled for the
+ * FIDO Metadata Service. This enables greater control over the types of authenticators that can
+ * interact with the Rely Party (a.k.a. "RP", a.k.a. "this server").
+ */
+if (ENABLE_CONFORMANCE === 'true') {
+  import('./fido-conformance').then(
+    ({ fidoRouteSuffix, fidoConformanceRouter }) => {
+      app.use(fidoRouteSuffix, fidoConformanceRouter);
+    },
+  );
+}
+
+/**
+ * RP ID represents the "scope" of websites on which a credential should be usable. The Origin
+ * represents the expected URL from which registration or authentication occurs.
+ */
+export const rpID = RP_ID;
+// This value is set at the bottom of page as part of server initialization (the empty string is
+// to appease TypeScript until we determine the expected origin based on whether or not HTTPS
+// support is enabled)
+export let expectedOrigin = '';
+
+/**
+ * 2FA and Passwordless WebAuthn flows expect you to be able to uniquely identify the user that
+ * performs registration or authentication. The user ID you specify here should be your internal,
+ * _unique_ ID for that user (uuid, etc...). Avoid using identifying information here, like email
+ * addresses, as it may be stored within the credential.
+ *
+ * Here, the example server assumes the following user has completed login:
+ */
+const loggedInUserId = 'internalUserId';
+
+const inMemoryUserDB: { [loggedInUserId: string]: LoggedInUser } = {
+  [loggedInUserId]: {
+    id: loggedInUserId,
+    username: `user@${rpID}`,
+    credentials: [],
+  },
+};
+
+/**
+ * Registration (a.k.a. "Registration")
+ */
+app.get('/generate-registration-options', async (req, res) => {
+  const user = inMemoryUserDB[loggedInUserId];
+
+  const {
+    /**
+     * The username can be a human-readable name, email, etc... as it is intended only for display.
+     */
+    username,
+    credentials,
+  } = user;
+
+  const opts: GenerateRegistrationOptionsOpts = {
+    rpName: 'SimpleWebAuthn Example',
+    rpID,
+    userName: username,
+    timeout: 60000,
+    attestationType: 'none',
+    /**
+     * Passing in a user's list of already-registered credential IDs here prevents users from
+     * registering the same authenticator multiple times. The authenticator will simply throw an
+     * error in the browser if it's asked to perform registration when it recognizes one of the
+     * credential ID's.
+     */
+    excludeCredentials: credentials.map((cred) => ({
+      id: cred.id,
+      type: 'public-key',
+      transports: cred.transports,
+    })),
+    authenticatorSelection: {
+      residentKey: 'discouraged',
+      /**
+       * Wondering why user verification isn't required? See here:
+       *
+       * https://passkeys.dev/docs/use-cases/bootstrapping/#a-note-about-user-verification
+       */
+      userVerification: 'preferred',
+    },
+    /**
+     * Support the two most common algorithms: ES256, and RS256
+     */
+    supportedAlgorithmIDs: [-7, -257],
+  };
+
+  const options = await generateRegistrationOptions(opts);
+
+  /**
+   * The server needs to temporarily remember this value for verification, so don't lose it until
+   * after you verify the registration response.
+   */
+  req.session.currentChallenge = options.challenge;
+
+  res.send(options);
+});
+
+app.post('/verify-registration', async (req, res) => {
+  const body: RegistrationResponseJSON = req.body;
+
+  const user = inMemoryUserDB[loggedInUserId];
+
+  const expectedChallenge = req.session.currentChallenge;
+
+  let verification: VerifiedRegistrationResponse;
+  try {
+    const opts: VerifyRegistrationResponseOpts = {
+      response: body,
+      expectedChallenge: `${expectedChallenge}`,
+      expectedOrigin,
+      expectedRPID: rpID,
+      requireUserVerification: false,
+    };
+    verification = await verifyRegistrationResponse(opts);
+  } catch (error) {
+    const _error = error as Error;
+    console.error(_error);
+    return res.status(400).send({ error: _error.message });
+  }
+
+  const { verified, registrationInfo } = verification;
+
+  if (verified && registrationInfo) {
+    const { credential } = registrationInfo;
+
+    const existingCredential = user.credentials.find((cred) => cred.id === credential.id);
+
+    if (!existingCredential) {
+      /**
+       * Add the returned credential to the user's list of credentials
+       */
+      const newCredential: WebAuthnCredential = {
+        id: credential.id,
+        publicKey: credential.publicKey,
+        counter: credential.counter,
+        transports: body.response.transports,
+      };
+      user.credentials.push(newCredential);
+    }
+  }
+
+  req.session.currentChallenge = undefined;
+
+  return res.send({ verified });
+});
+
+/**
+ * Login (a.k.a. "Authentication")
+ */
+app.get('/generate-authentication-options', async (req, res) => {
+  // You need to know the user by this point
+  const user = inMemoryUserDB[loggedInUserId];
+
+  const opts: GenerateAuthenticationOptionsOpts = {
+    timeout: 60000,
+    allowCredentials: user.credentials.map((cred) => ({
+      id: cred.id,
+      type: 'public-key',
+      transports: cred.transports,
+    })),
+    /**
+     * Wondering why user verification isn't required? See here:
+     *
+     * https://passkeys.dev/docs/use-cases/bootstrapping/#a-note-about-user-verification
+     */
+    userVerification: 'preferred',
+    rpID,
+  };
+
+  const options = await generateAuthenticationOptions(opts);
+
+  /**
+   * The server needs to temporarily remember this value for verification, so don't lose it until
+   * after you verify the authentication response.
+   */
+  req.session.currentChallenge = options.challenge;
+
+  res.send(options);
+});
+
+app.post('/verify-authentication', async (req, res) => {
+  const body: AuthenticationResponseJSON = req.body;
+
+  const user = inMemoryUserDB[loggedInUserId];
+
+  const expectedChallenge = req.session.currentChallenge;
+
+  let dbCredential: WebAuthnCredential | undefined;
+  // "Query the DB" here for a credential matching `cred.id`
+  for (const cred of user.credentials) {
+    if (cred.id === body.id) {
+      dbCredential = cred;
+      break;
+    }
+  }
+
+  if (!dbCredential) {
+    return res.status(400).send({
+      error: 'Authenticator is not registered with this site',
+    });
+  }
+
+  let verification: VerifiedAuthenticationResponse;
+  try {
+    const opts: VerifyAuthenticationResponseOpts = {
+      response: body,
+      expectedChallenge: `${expectedChallenge}`,
+      expectedOrigin,
+      expectedRPID: rpID,
+      credential: dbCredential,
+      requireUserVerification: false,
+    };
+    verification = await verifyAuthenticationResponse(opts);
+  } catch (error) {
+    const _error = error as Error;
+    console.error(_error);
+    return res.status(400).send({ error: _error.message });
+  }
+
+  const { verified, authenticationInfo } = verification;
+
+  if (verified) {
+    // Update the credential's counter in the DB to the newest count in the authentication
+    dbCredential.counter = authenticationInfo.newCounter;
+  }
+
+  req.session.currentChallenge = undefined;
+
+  return res.send({ verified });
+});
